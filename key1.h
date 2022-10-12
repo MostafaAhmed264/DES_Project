75622ebfe68103dce181400194b39929bc756e80
Mostafa A. Tawfik <85037618+MostafaAhmed264@users.noreply.github.com>
2022-10-13 01:51:21 +0200

    Version1_Encrypt_No_Decrypt

--- key1.h
+++ key1.h
@@ -1,4 +1,6 @@
 typedef unsigned long int uint64;
 typedef unsigned int uint32;
+
 void divideValue64(uint64 input, uint32 *leftOutput, uint32 *rightOutput);
 void divideValue56(uint64 input, uint32 *leftOutput1, uint32 *rightOutput1);
+void gen_keys (uint64 * key_arr, uint32 key_left_28, uint32 key_right_28, int * rotate_table);

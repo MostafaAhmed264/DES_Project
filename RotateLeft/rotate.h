87bbcec5051b14d14bfd4cb95699344c73791cb1
mostafaabbas74x <85040485+mostafaabbas74x@users.noreply.github.com>
2022-10-12 22:47:08 +0200

    Update rotate.h

--- RotateLeft/rotate.h
+++ RotateLeft/rotate.h
@@ -3,9 +3,9 @@ typedef unsigned int uint32;
 #define KEY_32_BITS             //full size of the key passed to the function 
 #ifdef KEY_64_BITS
 #define KEY_FULL_SIZE 64
+uint64 rotate(uint64 *  key, int sizeOfKey, int rotationAmount);
 #endif
 #ifdef KEY_32_BITS
 #define KEY_FULL_SIZE 32
-#endif
-uint64 rotate(uint64 *  key, int sizeOfKey, int rotationAmount);
 uint32 rotate(uint32 *  key, int sizeOfKey, int rotationAmount);
+#endif

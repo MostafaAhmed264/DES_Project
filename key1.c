c77065921041d53656c66ec239cb35188bad52d1
ZiadFahmy <68162606+zyadfahmy2@users.noreply.github.com>
2022-10-12 23:17:24 +0200

    adding .h

--- key1.c
+++ key1.c
@@ -1,6 +1,5 @@
-typedef unsigned long int uint64;
-typedef unsigned int uint32;
-#include <stdio.h>
+
+#include "key1.h"
 
 void divideValue64(uint64 input, uint32 *leftOutput, uint32 *rightOutput)
 {
@@ -13,20 +12,20 @@ void divideValue56(uint64 input, uint32 *leftOutput1, uint32 *rightOutput1)
     *rightOutput1 = (uint32)(input & 0x0000000FFFFFFFLL);
 }
 
-int main(void)
-{
-    uint64 x = 0xFFFFFFFF300330FF;
-    uint64 y = 0xFFFFF0F1FFFFFF;
+// int main(void)
+// {
+//     uint64 x = 0xFFFFFFFF300330FF;
+//     uint64 y = 0xFFFFF0F1FFFFFF;
 
-    uint32 read1;
-    uint32 read2;
-    uint32 key1;
-    uint32 key2;
-    divideValue64(x, &read1, &read2);
-    divideValue56(y, &key1, &key2);
+//     uint32 read1;
+//     uint32 read2;
+//     uint32 key1;
+//     uint32 key2;
+//     divideValue64(x, &read1, &read2);
+//     divideValue56(y, &key1, &key2);
 
-    printf("%x\n", read1);
-    printf("%x\n", read2);
-    printf("%x\n", key1);
-    printf("%x\n", key2);
-}
+//     printf("%x\n", read1);
+//     printf("%x\n", read2);
+//     printf("%x\n", key1);
+//     printf("%x\n", key2);
+// }
---
+++ key1.h
@@ -0,0 +1,4 @@
+typedef unsigned long int uint64;
+typedef unsigned int uint32;
+void divideValue64(uint64 input, uint32 *leftOutput, uint32 *rightOutput);
+void divideValue56(uint64 input, uint32 *leftOutput1, uint32 *rightOutput1);
